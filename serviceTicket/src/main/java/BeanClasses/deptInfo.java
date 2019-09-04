82d2747d6e2b0ab059ab1b5315715ca675d53f61
mdluqman <mdluqman678@gmail.com>
2019-09-04 17:02:26 +0530

    Second Commit

--- serviceTicket/src/main/java/BeanClasses/deptInfo.java
+++ serviceTicket/src/main/java/BeanClasses/deptInfo.java
@@ -1,12 +1,7 @@
 package BeanClasses;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
